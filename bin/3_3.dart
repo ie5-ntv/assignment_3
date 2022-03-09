da795b55e1ccf56e76a644477c590d75ef6acac1
edduson <bingibergs@gmail.com>
2022-03-09 12:04:07 +0000

    mjakast lengra og lengra

--- bin/3_3.dart
+++ bin/3_3.dart
@@ -1,3 +1,12 @@
-//3. Write a program to read 5 numbers from keyboard, using a loop.The program
-// should work with any 5 numbers. Program ends by printing out the sum and
-// average of the numbers.
+void Item_3() {
+//3. Write a program to read 5 numbers from keyboard,
+// using any loop. The program should work with any 5 numbers.
+// Program ends by printing out the
+num sum = 1;
+num average = 1;
+// TODO read 5 numbers from keyboard, // using any loop.
+// The program should work with any 5 numbers.
+// also sums and averages using dart.math ?
+print ('sum: $sum'); // Program ends by printing out the sum
+print ('Average: $average'); // and average of the numbers.
+}
