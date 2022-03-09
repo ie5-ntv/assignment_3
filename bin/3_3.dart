c452ab7dd646ed3ac83e82e7304f71f3804cfe0d
edduson <bingibergs@gmail.com>
2022-03-09 15:25:00 +0000

    DONE: 3_1, 3_2 WIP: 3_3, 3_4

--- bin/3_3.dart
+++ bin/3_3.dart
@@ -1,10 +1,12 @@
-void Item_3() {
+void main() {
 //3. Write a program to read 5 numbers from keyboard,
 // using any loop. The program should work with any 5 numbers.
 // Program ends by printing out the
 num sum = 1;
 num average = 1;
 // TODO read 5 numbers from keyboard, // using any loop.
+  int userNum = int.parse(stdin.readLineSync()!); // TODO: here we go...
+
 // The program should work with any 5 numbers.
 // also sums and averages using dart.math ?
 print ('sum: $sum'); // Program ends by printing out the sum
