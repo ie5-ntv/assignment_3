0943263d894753dc246bb3253a1634038bccef6f
ie5-ntv <101134776+ie5-ntv@users.noreply.github.com>
2022-03-09 21:23:41 +0000

    final

--- bin/3_3.dart
+++ bin/3_3.dart
@@ -5,16 +5,17 @@ void main() {
 // Program ends by printing out the
 num sum = 0;
 num average = 0;
-// TODO read 5 numbers from keyboard using any loop.
-  for(int i; i<5; i++) {
-    if i == 0 print ('Enter a number:') // ask for first number
-      int userNum = int.parse(stdin.readLineSync()!); // take a number
-    sum == sum + userNum
-  else if i > 0 print('Enter another number:') // ask for second to fourth number
-  else if i == 4 print('Enter the last number: ') // ak for fifth number
+for (int i = 0; i<5; i++){
+  if (i == 0) {
+      print('Enter a number:');} // ask for first number
+  else if (i < 4) {
+      print('Enter another number:');} // ask for second to fourth number
+  else {
+      print('Enter the last number: ');} // ask for fifth number
   int userNum = int.parse(stdin.readLineSync()!); // take a number
-  sum == sum + userNum}
-// The program should work with any 5 numbers.
+  sum = (sum + userNum); // add taken number to sum
+  }
 print ('sum: $sum'); // Program ends by printing out the sum
+average = sum / 5;
 print ('Average: $average'); // and average of the numbers.
 }
--- bin/3_4.dart
+++ bin/3_4.dart
@@ -1,8 +1,11 @@
 //4. Write a program that accepts a number from the user and counts the
 // number of digits in the given integer using loop..
+import 'dart:io';
+void main(){
 int userNum = int.parse(stdin.readLineSync()!); // take a number
-// Example Input •Input num: 24967
 var userDigits = 0; // (b) Initialize another variable to store total digits say digit = 0.
-while (num != 0 ; userDigits++){userNum = usernum / 10;}
-// (e) do step c and d while num > 0 or num != 0.
-print('Number of digits: $userDigits')
+while (userNum != 0){
+  userNum = (userNum / 10).floor();
+  userDigits++;
+}
+print('Number of digits: $userDigits');}
